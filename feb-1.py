ab52c7e637dd16835329647756fbaeed1e499b32
manasii16 <115860448+manasii16@users.noreply.github.com>
2024-02-07 20:47:00 +0530

    Update feb-1.py

--- feb-1.py
+++ feb-1.py
@@ -127,6 +127,32 @@ class Solution(object):
         # Convert the character array back to a string and return the result.
         return "".join(arr)
     
-    
+--------------------------------------------------------------------------------------------------------------------------------
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+# Return the maximum amount of water a container can store.
+
+
+class Solution(object):
+    def maxArea(self, height):
+        left=0
+        right=len(height)-1
+        maxsize=0
+        
+        while left<right:
+            currarea=min(height[left],height[right])*(right-left)
+            maxsize=max(maxsize,currarea)
+            
+            if (height[left]<height[right]):
+                left+=1
+            else:
+                right-=1
+        return maxsize
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+
     
         
